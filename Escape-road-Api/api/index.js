f3e9618b823867f8a84d6017df4562347799daa6
WH <710569885@qq.com>
2025-04-15 10:27:15 +0800

    创建2

---
+++ Escape-road-Api/api/index.js
@@ -0,0 +1,9 @@
+import {
+  adminLogin,
+  verifyAdminToken,
+  changeAdminPassword,
+  getAllComments,
+  deleteCommentById
+} from './admin.js';
+
+console.log("[API] index.js loaded successfully."); 
